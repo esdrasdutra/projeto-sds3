63a083ad64b9a45ebea78c1413892a4246b0457b
esdrasdutra <esdrasfdutra@gmail.com>
2021-05-05 13:09:11 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,33 @@
-import NavBar from    './components/NavBar';
-import Footer from    './components/Footer';
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 import DataTable from './components/DataTable';
+import BarChart from './components/BarChart';
+import DonutChart from './components/DonutsChart';
 
 function App() {
   return (
     <>
       <NavBar />
       <div className="container">
-        <h1 className="text-primary">Olá, Semana Spring React!</h1>
-        <DataTable/>
+        <h1 className="texto-primary py-3">Dashboard de Vendas</h1>
+        <div className="row px-3">
+          <div className="col-sm-6">
+            <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+            <BarChart/>
+          </div>
+          <div className="col-sm-6">
+            <h5 className="text-center text-success">Taxa de Sucesso</h5>
+            <DonutChart/>
+          </div>
+
+          <div className="py-3">
+            <h2 className="texto-primary"> Todas as Vendas</h2>
+          </div>
+
+        </div>
+        <DataTable />
       </div>
-      <Footer/>
+      <Footer />
     </>
   );
 }
