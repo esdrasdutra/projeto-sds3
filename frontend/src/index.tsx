7ef0ab4988e9ac53263a23f1962cbd88f435b579
esdrasdutra <esdrasfdutra@gmail.com>
2021-05-06 08:03:18 -0300

    Domain model, database seed

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/css/styles.css';
+import 'assets/css/styles.css';
 import 'bootstrap/dist/css/bootstrap.css';
 
 import App from './App';
