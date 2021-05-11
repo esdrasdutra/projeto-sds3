e9308762153838e3b4af4b60ddb2822f5fcdaf01
esdrasdutra <esdrasfdutra@gmail.com>
2021-05-11 10:09:01 -0300

    Dashboard

--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -17,22 +17,24 @@ export default class Dashboard extends React.Component<IDashboardProps, IDashboa
         <div className="container">
           <div className="row px-6">
             <div className="col-6">
-              <h1 className="texto-primary">Dashboard de Vendas</h1>
+              <h1 className="texto-primary">Dashboard</h1>
             </div>         
           </div>
 
           <div className="row px-3">
             <div className="col-sm-6">
-              <h5 className="text-center text-secondary">Taxa de Sucesso</h5>
+              <h5 className="text-center text-secondary">Taxa de SUCESSO</h5>
+              <p className="text-center">Quantas VISITAS foram convertidas em NEGÓCIO FECHADO?</p>
               <BarChart />
             </div>
             <div className="col-sm-6">
-              <h5 className="text-center text-success">Taxa de Sucesso</h5>
+              <h5 className="text-center text-success">Total de VENDAS</h5>
+              <p className="text-center">VALOR TOTAL de NEGÓCIOS FECHADOS</p>
               <DonutChart />
             </div>
 
             <div className="py-3">
-              <h2 className="texto-primary"> Todas as Vendas</h2>
+              <h1 className="texto-primary"> Todas as Vendas</h1>
             </div>
 
           </div>
